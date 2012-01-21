1a51298e32c866cc1f19bb4d8b1f0c65448aabd3
Court Ewing <court@epixa.com>
2012-01-21 15:29:50 -0500

    Added memory-based session support for sustaining user authentication and authorization between page requests

--- public/js/game.js
+++ public/js/game.js
@@ -12,20 +12,44 @@ nsfw.cards = nsfw.cards || {};
             throw "Game id not specified in game config";
         }
 
-        now.joinGame(config.id);
+        this.id = config.id;
+
+        now.join(config.id);
     };
 
 
     /**
+     * Alert to an error
+     *
+     * @param message
+     */
+    now.error = function(message){
+        var error = 'Error: ' + message;
+        console.log(error);
+        alert(error);
+    };
+
+    /**
      * Indicates that the given user has left the current game
      *
-     * @events
-     *  game.playerLeft
+     * @triggers game.playerLeft
      *
      * @param user
      */
     now.playerLeft = function(user){
-        console.log('Triggering game.playerLeft for player ' + user.clientId);
+        console.log('Triggering game.playerLeft for player ' + user.name);
         $(document).trigger('game.playerLeft', [user]);
+    };
+
+    /**
+     * Indicates taht the given user has joined the current game
+     *
+     * @triggers game.playerJoined
+     *
+     * @param user
+     */
+    now.playerJoined = function(user){
+        console.log('Triggering game.playerJoined for player ' + user.name);
+        $(document).trigger('game.playerJoined', [user]);
     };
 })(jQuery);
