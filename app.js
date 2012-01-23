1c7d4b0f490019c8da275d81a16b6d659e350286
Court Ewing <court@epixa.com>
2012-01-23 17:19:05 -0500

    Removed now.js and added socket.io

--- app.js
+++ app.js
@@ -7,12 +7,18 @@
 
 // Module dependencies
 var express = require('express');
-var nowjs = require('now');
+var connect = require('connect');
+
+var io = require('socket.io');
 
 
 // Initialize app
 var app = module.exports = express.createServer().listen(3000);
-var everyone = nowjs.initialize(app);
+var sio = io.listen(app);
+
+
+var MemoryStore = express.session.MemoryStore;
+var sessionStore = new MemoryStore();
 
 
 // Configuration app
@@ -20,7 +26,8 @@ app.configure(function(){
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
     app.use(express.bodyParser());
-    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: 'Sl18TAiM4B49g9CD1TK9oVJIyoH63Sdq', store: sessionStore }));
     app.use(app.router);
     app.use(express.static(__dirname + '/public'));
 });
@@ -34,18 +41,44 @@ app.configure('production', function(){
 });
 
 
-everyone.now.joinGame = function(gameId){
-    this.user.game = gameId;
-    nowjs.getGroup(this.user.game).addUser(this.user.clientId);
-    console.log(this.user.clientId + ' joined game ' + this.user.game);
-};
-
-everyone.on('leave', function(){
-    if (this.user.game) {
-        var group = nowjs.getGroup(this.user.game);
-        group.now.playerLeft(this.user);
-        console.log(this.user.clientId + ' left game ' + this.user.game);
+// When a new websocket opens, set up a session object that we can access on future calls
+// through that same websocket
+sio.set('authorization', function(data, accept){
+    if (data.headers.cookie) {
+        data.cookie = connect.utils.parseCookie(data.headers.cookie);
+        data.sessionID = data.cookie['connect.sid'];
+        data.sessionStore = sessionStore;
+        sessionStore.get(data.sessionID, function(error, session){
+            if (error || !session) {
+                accept('Failed to read session information', false);
+            } else {
+                data.session = new connect.middleware.session.Session(data, session);
+                accept(null, true);
+            }
+        });
+    } else {
+        return accept('No cookie transmitted.', false);
     }
+});
+
+// Express sessions will timeout if there hasn't been a new HTTP request after a certain period of time.
+// Since most of our functionality is handled through an open websocket rather than HTTP requests,
+// we make sure to keep the session alive.
+sio.sockets.on('connection', function(socket){
+    var handshake = socket.handshake;
+
+    console.log('A socket with sessionID ' + handshake.sessionID + ' connected.');
+
+    var intervalID = setInterval(function(){
+        handshake.session.reload(function(){
+            handshake.session.touch().save();
+        });
+    }, 60 * 1000);
+
+    socket.on('disconnect', function(){
+        console.log('A socket with sessionID ' + handshake.sessionID + ' disconnected.');
+        clearInterval(intervalID);
+    });
 });
 
 
